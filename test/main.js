6b75c80c7f4eb26c0d042137e19b1eaef7cd1c0e
David Banks <amoebae@gmail.com>
2018-01-18 13:17:56 +0000

    adapting a technique for testing d3

--- test/main.js
+++ test/main.js
@@ -1,6 +1,29 @@
 // main.js
 
+import * as d3 from 'd3';
+
+var c;
+
+function drawBarChart() {
+    var that = {};
+    that.render = function() {
+        var svg = d3.select('body')
+            .append('svg')
+            .attr('height', '500')
+            .attr('width', '500')
+            .append('g')
+            .attr('transform', "translate(0, 0)");
+    };
+
+    return that;
+}
+
 var assert = require('chai').assert;
+
+beforeEach(function() {
+    c = drawBarChart();
+    c.render();
+});
 
 it('is functioning', function () {
     assert.equal(4, 2 + 2);
