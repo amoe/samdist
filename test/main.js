42452fd47b6806ffc6f7cd61fed5a3661b1ce7b8
David Banks <amoebae@gmail.com>
2018-01-18 13:35:09 +0000

    separating out the system under test

--- test/main.js
+++ test/main.js
@@ -1,22 +1,10 @@
 // main.js
 
 import * as d3 from 'd3';
+import foo from '../src/bar-chart.js'
 
 var c;
 
-function drawBarChart() {
-    var that = {};
-    that.render = function() {
-        var svg = d3.select('body')
-            .append('svg')
-            .attr('height', '500')
-            .attr('width', '500')
-            .append('g')
-            .attr('transform', "translate(0, 0)");
-    };
-
-    return that;
-}
 
 function getSvg() {
     return d3.select('svg');
@@ -24,10 +12,14 @@ function getSvg() {
 
 var assert = require('chai').assert;
 
-// beforeEach(function() {
-//     c = drawBarChart();
-//     c.render();
-// });
+beforeEach(function() {
+    c = foo.drawBarChart();
+    c.render();
+});
+
+afterEach(function() {
+    d3.selectAll('svg').remove();
+});
 
 it('can be created', function () {
     const result = getSvg();
