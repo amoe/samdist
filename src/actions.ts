852d1e86c7f6c8ddf630b4159bef0a957d5c6cdb
David Banks <amoebae@gmail.com>
2018-01-24 11:45:46 +0000

    speccing format for bag of words

--- src/actions.ts
+++ src/actions.ts
@@ -1,11 +1,25 @@
 // actions.ts
 
+import axios from 'axios';
+
+const API_PREFIX = "/api";
+
 const actions = {
     increment(context) {
         context.commit('increment')
     },
-    submitBagOfWordsRequest() {
+    submitBagOfWordsRequest(payload) {
         console.log("submitting bag of words request");
+        axios.get(API_PREFIX + "/bag-of-words", {
+            params: {
+                field: payload.field,
+                cutoff: payload.cutoff
+            }
+        }).then(function (response) {
+            console.log("regular response");
+        }).catch(function (error) {
+            console.log("error response");
+        });
     }
 };
 
