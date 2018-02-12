e0fc6d4c6012084a3a5e8bc700f02114e4b6ea1a
David Banks <amoebae@gmail.com>
2018-02-12 12:33:08 +0000

    deploying frontend

--- fabfile.py
+++ fabfile.py
@@ -9,10 +9,22 @@ import hashlib
 # We really need one dir to contain the python stuff
 
 DEPLOYMENT_TARGET_DIRECTORY = '/usr/local/lib/samdist'
+DEPLOYMENT_TARGET_DIRECTORY_FE = '/srv/http/samdist'
 
 # Nice two layers of indirection going on here ;)
 def build():
     local("yarn build")
+
+def deploy_frontend():
+    temporary_path = generate_temporary_directory(DEPLOYMENT_TARGET_DIRECTORY_FE)
+    with hide('running'):
+        local(
+            "rsync -a --delete --exclude-from=exclude.rsf ./ %s:%s/" \
+              % (env['host_string'], temporary_path)
+        )
+    sudo("mkdir -p %s" % (DEPLOYMENT_TARGET_DIRECTORY_FE,))
+    sudo("rsync -rtv --delete %s/ %s/" % (temporary_path, DEPLOYMENT_TARGET_DIRECTORY_FE))
+    with hide('running'): run("rm -rf %s" % temporary_path)
 
 # because we can't combine rsync and sudo, we have to do this fairly silly
 # rigmarole        
