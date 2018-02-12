789e55721c366bdab9d1b3b716f9199c6453a2e4
David Banks <amoebae@gmail.com>
2018-02-12 12:37:04 +0000

    fixing deployment procedure

--- fabfile.py
+++ fabfile.py
@@ -15,30 +15,24 @@ DEPLOYMENT_TARGET_DIRECTORY_FE = '/srv/http/samdist'
 def build():
     local("yarn build")
 
-def deploy_frontend():
-    temporary_path = generate_temporary_directory(DEPLOYMENT_TARGET_DIRECTORY_FE)
-    with hide('running'):
-        local(
-            "rsync -a --delete --exclude-from=exclude.rsf ./ %s:%s/" \
-              % (env['host_string'], temporary_path)
-        )
-    sudo("mkdir -p %s" % (DEPLOYMENT_TARGET_DIRECTORY_FE,))
-    sudo("rsync -rtv --delete %s/ %s/" % (temporary_path, DEPLOYMENT_TARGET_DIRECTORY_FE))
-    with hide('running'): run("rm -rf %s" % temporary_path)
-
 # because we can't combine rsync and sudo, we have to do this fairly silly
 # rigmarole        
-def deploy_backend():
-    temporary_path = generate_temporary_directory(DEPLOYMENT_TARGET_DIRECTORY)
+def deploy(source, target):
+    temporary_path = generate_temporary_directory(target)
     with hide('running'):
         local(
-            "rsync -a --delete --exclude-from=exclude.rsf ./ %s:%s/" \
-              % (env['host_string'], temporary_path)
+            "rsync -a --delete --exclude-from=exclude.rsf %s/ %s:%s/" \
+              % (source, env['host_string'], temporary_path)
         )
-    sudo("mkdir -p %s" % (DEPLOYMENT_TARGET_DIRECTORY,))
-    sudo("rsync -rtv --delete %s/ %s/" % (temporary_path, DEPLOYMENT_TARGET_DIRECTORY))
+    sudo("mkdir -p %s" % (target,))
+    sudo("rsync -rtv --delete %s/ %s/" % (temporary_path, target))
     with hide('running'): run("rm -rf %s" % temporary_path)
+    
+def deploy_frontend():
+    deploy('./dist', DEPLOYMENT_TARGET_DIRECTORY_FE)
 
+def deploy_backend():
+    deploy(".", DEPLOYMENT_TARGET_DIRECTORY)
    
 # algorithm copied from fabric core code for 'put' with 'use_sudo' kwargs
 def generate_temporary_directory(remote_path):
