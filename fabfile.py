a92e7d4849214f13e79c03db3fbd0b3c4774add3
David Banks <amoebae@gmail.com>
2018-02-12 12:29:41 +0000

    more deployment fudge

--- fabfile.py
+++ fabfile.py
@@ -6,11 +6,17 @@ from fabric.api \
 import time
 import hashlib
 
-DEPLOYMENT_TARGET_DIRECTORY = '/srv/http/samdist'
+# We really need one dir to contain the python stuff
+
+DEPLOYMENT_TARGET_DIRECTORY = '/usr/local/lib/samdist'
+
+# Nice two layers of indirection going on here ;)
+def build():
+    local("yarn build")
 
 # because we can't combine rsync and sudo, we have to do this fairly silly
 # rigmarole        
-def deploy():
+def deploy_backend():
     temporary_path = generate_temporary_directory(DEPLOYMENT_TARGET_DIRECTORY)
     with hide('running'):
         local(
@@ -20,6 +26,7 @@ def deploy():
     sudo("mkdir -p %s" % (DEPLOYMENT_TARGET_DIRECTORY,))
     sudo("rsync -rtv --delete %s/ %s/" % (temporary_path, DEPLOYMENT_TARGET_DIRECTORY))
     with hide('running'): run("rm -rf %s" % temporary_path)
+
    
 # algorithm copied from fabric core code for 'put' with 'use_sudo' kwargs
 def generate_temporary_directory(remote_path):
