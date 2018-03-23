20a1fc3b64e6ca9fec2dbbf16963ab36dad10495
David Banks <amoebae@gmail.com>
2018-03-23 15:52:56 +0000

    moving tick generator into barchart mod

--- src/utility.ts
+++ src/utility.ts
@@ -39,17 +39,6 @@ const functions = {
         }
         console.log(error.config);
     },
-    generateTicks(min: number, max: number, nTicks: number) {
-        const sizeOfRange = max - min;
-        const roundTo = sizeOfRange / nTicks;
-
-        const realMin = floorN(min, roundTo);
-        const realMax = ceilingN(max, roundTo);
-        
-        const step = (realMax  - realMin) / nTicks;
-
-        return _.range(1, nTicks).map(n => realMin + (step * n));
-    }
 };
 
 export default functions;
