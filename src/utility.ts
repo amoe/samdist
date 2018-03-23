f39cd82b41a146b19be55527c59aeded82ed70f2
David Banks <amoebae@gmail.com>
2018-03-23 15:38:17 +0000

    tests for tick generator

--- src/utility.ts
+++ src/utility.ts
@@ -1,4 +1,13 @@
 import {ChartDrawRequest} from './interfaces';
+import _ from 'lodash';
+
+function ceilingN(x: number, n: number) {
+    return Math.ceil(x / n) * n;
+}
+
+function floorN(x: number, n: number) {
+    return Math.floor(x / n) * n;
+}
 
 const functions = {
     getChartDrawRequest(data: any[]): ChartDrawRequest {
@@ -29,6 +38,17 @@ const functions = {
             console.log('Error', error.message);
         }
         console.log(error.config);
+    },
+    generateTicks(min: number, max: number, nTicks: number) {
+        const sizeOfRange = max - min;
+        const roundTo = sizeOfRange / nTicks;
+
+        const realMin = floorN(min, roundTo);
+        const realMax = ceilingN(max, roundTo);
+        
+        const step = (realMax  - realMin) / nTicks;
+
+        return _.range(1, nTicks).map(n => realMin + (step * n));
     }
 };
 
