babd726a9b49139f0baa72fe029af06667387e7b
David Banks <amoebae@gmail.com>
2018-04-05 12:58:00 +0100

    refactoring

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-FLASK_APP=main.py FLASK_DEBUG=1 flask run
+FLASK_APP=samuels_rest_server.py FLASK_DEBUG=1 flask run
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import main
+import samuels_rest_server
 
 def test_arithmetic_works():
     assert 2+2 == 4
